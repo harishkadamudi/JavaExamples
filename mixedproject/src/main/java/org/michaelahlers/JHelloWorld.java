1cb7372885ea670b9b518429e092ce64d5e90a1e
Harish Kadamudi <harishyouk@gmail.com>
2015-12-09 18:29:56 +0530

    Initial commit

--- mixedproject/src/main/java/org/michaelahlers/JHelloWorld.java
+++ mixedproject/src/main/java/org/michaelahlers/JHelloWorld.java
@@ -1,12 +1,12 @@
 package org.michaelahlers;
 
 public class JHelloWorld {
-	public void printMessage() {
-		System.out.println("Hello from Java!");
-	}
+  public void printMessage() {
+    System.out.println("Hello from Java!");
+  }
 
-	public static void main(final String... arguments) {
-		new JHelloWorld().printMessage();
-		new SHelloWorld().printMessage();
-	}
+  public static void main(final String... arguments) {
+    new JHelloWorld().printMessage();
+    new SHelloWorld().printMessage();
+  }
 }
